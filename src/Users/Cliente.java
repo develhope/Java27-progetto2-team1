de1c40f1ba3762e09b86fbba2eae219614c954e0
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-09-04 19:22:56 +0200

    Rimossi throws da classe Cliente

--- src/Users/Cliente.java
+++ src/Users/Cliente.java
@@ -1,54 +1,46 @@
 package Users;
 
 import Enums.Roles;
-import Exceptions.CarrelloVuotoException;
-import Exceptions.ProdottoNonTrovatoException;
 import Management.Carrello;
 import Products.ProdottoElettronicoUtente;
-
-import java.io.IOException;
 import java.util.Set;
 
 public class Cliente extends Utente {
 
     private final Carrello carrelloCliente = new Carrello();
 
-    public Cliente(){};
+    public Cliente(){}
 
     public Cliente(String nome, String cognome, int age, String email, String password) {
         super(nome, cognome, age, email, password);
         role = Roles.CLIENTE;
     }
 
-    public boolean login(String emailCliente, String passwordCliente){
-	    return emailCliente.equals(getEmail()) && passwordCliente.equals(getPassword());
-    }
-
-    public void aggiungiProdottoAlCarrello( ProdottoElettronicoUtente prodotto, int quantita) throws ProdottoNonTrovatoException, IOException {
+    public void aggiungiProdottoAlCarrello( ProdottoElettronicoUtente prodotto, int quantita){
         carrelloCliente.aggiungiProdotto(prodotto, quantita);
     }
 
-    public ProdottoElettronicoUtente ricercaTramiteId(int id) throws ProdottoNonTrovatoException{
+    public ProdottoElettronicoUtente ricercaTramiteId(int id){
         return carrelloCliente.ricercaPerId(id);
     }
 
-    public Set<ProdottoElettronicoUtente> ricercaProdottoPerMarca(String marca) throws ProdottoNonTrovatoException {
+    public Set<ProdottoElettronicoUtente> ricercaProdottoPerMarca(String marca){
         return carrelloCliente.ricercaPerMarca(marca);
     }
 
-    public Set<ProdottoElettronicoUtente> ricercaProdottoPerModello(String modello) throws ProdottoNonTrovatoException {
+    public Set<ProdottoElettronicoUtente> ricercaProdottoPerModello(String modello){
         return carrelloCliente.ricercaPerModello(modello);
     }
 
-    public Set<ProdottoElettronicoUtente> ricercaProdottoPerPrezzoDiVendita(double prezzo) throws ProdottoNonTrovatoException {
+    public Set<ProdottoElettronicoUtente> ricercaProdottoPerPrezzoDiVendita(double prezzo){
         return carrelloCliente.ricercaPerPrezzoVendita(prezzo);
     }
 
-    public Set<ProdottoElettronicoUtente> ricercaProdottoPerRange(double prezzoMin, double prezzoMax) throws ProdottoNonTrovatoException {
+    public Set<ProdottoElettronicoUtente> ricercaProdottoPerRange(double prezzoMin, double prezzoMax){
         return carrelloCliente.ricercaPerRange(prezzoMin, prezzoMax);
     }
 
-    public Set<ProdottoElettronicoUtente> ricercaProdottoPerTIpo(String tipo) throws ProdottoNonTrovatoException {
+    public Set<ProdottoElettronicoUtente> ricercaProdottoPerTIpo(String tipo){
         return carrelloCliente.ricercaPerTipo(tipo);
     }
 
@@ -56,19 +48,19 @@ public class Cliente extends Utente {
         carrelloCliente.stampaCarrello();
     }
 
-    public void rimuoviProdottoTramiteId(int id, int quantita) throws ProdottoNonTrovatoException, IOException {
-	    carrelloCliente.rimozioneTramiteId(id, quantita);
+    public void rimuoviProdottoTramiteId(int id, int quantita){
+        carrelloCliente.rimozioneTramiteId(id, quantita);
     }
 
-    public double calcoloTotaleCarrello() throws CarrelloVuotoException {
-        return carrelloCliente.calcoloTot();
+    public double calcoloTotaleCarrello(){
+        return  carrelloCliente.calcoloTot();
     }
 
-    public void svuotaCarrelloProdotti() throws IOException {
+    public void svuotaCarrelloProdotti(){
         carrelloCliente.svuotaCarrello();
     }
 
-    public void concludiAcquistoProdotti() throws CarrelloVuotoException, IOException {
+    public void concludiAcquistoProdotti(){
         carrelloCliente.concludiAcquisto();
     }
 
