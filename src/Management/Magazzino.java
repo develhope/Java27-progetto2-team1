35c0d33ad02d76bf80397acbf2477fad3129ba29
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-09-04 19:21:37 +0200

    Aggiunto hanlder a classe Magazzino

--- src/Management/Magazzino.java
+++ src/Management/Magazzino.java
@@ -1,7 +1,9 @@
 package Management;
+import Exceptions.ExceptionHandler;
 import Exceptions.ProdottoNonTrovatoException;
 import Products.ProdottoElettronico;
 import Utility.MagazzinoReader;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,16 +11,10 @@ import java.util.stream.Collectors;
 public class Magazzino {
 
     //Accetta tipi Products.ProdottoElettronico
-
     private Set<ProdottoElettronico> magazzino;
 
     public Magazzino() {
-        try{
-            this.magazzino = MagazzinoReader.leggiMagazzinoDaFile();
-        } catch (IOException e){
-            System.err.println(e.getMessage());
-        }
-
+        magazzino = new HashSet<>();
     }
 
     //Ritorna il totale degli articoli presenti in magazzino.
@@ -29,9 +25,9 @@ public class Magazzino {
 
     //Lista filtrata per tipo
     public Set<ProdottoElettronico> filtredBytype(String type){
-       return magazzino.stream()
-               .filter(d->d.getTipoElettronico().toString().equalsIgnoreCase(type))
-               .collect(Collectors.toSet());
+        return magazzino.stream()
+                .filter(d->d.getTipoElettronico().toString().equalsIgnoreCase(type))
+                .collect(Collectors.toSet());
     }
     public Set<ProdottoElettronico> filtredByModel(String type){
         return magazzino.stream()
@@ -39,9 +35,9 @@ public class Magazzino {
                 .collect(Collectors.toSet());
     }
     public Set<ProdottoElettronico> filtredByProducer(String type){
-       return magazzino.stream()
-               .filter(d->d.getMarca().equalsIgnoreCase(type))
-               .collect(Collectors.toSet());
+        return magazzino.stream()
+                .filter(d->d.getMarca().equalsIgnoreCase(type))
+                .collect(Collectors.toSet());
     }
     public Set<ProdottoElettronico> filtredBySellPrice(double price){
         return magazzino.stream()
@@ -59,31 +55,34 @@ public class Magazzino {
                 .filter(d->d.getPrezzoVendita() > price && d.getPrezzoVendita() < secondPrice)
                 .collect(Collectors.toSet());
     }
-    public void addProductToMagazzino(ProdottoElettronico dispositivo) throws IOException {
+    public void addProductToMagazzino(ProdottoElettronico dispositivo){
         boolean found = magazzino.stream().anyMatch(d->d.getId() == dispositivo.getId());
         if(found){
             dispositivo.setQuantitaMagazzino(dispositivo.getQuantitaMagazzino() + 1);
             MagazzinoReader.aggiornaMagazzino(magazzino);
         } else{
             MagazzinoReader.aggiungiProdottoAlMagazzino(dispositivo);
-            magazzino = MagazzinoReader.leggiMagazzinoDaFile();
+            MagazzinoReader.leggiMagazzinoDaFile();
         }
     }
 
-    public void removeProductFromMagazzino(int id) throws ProdottoNonTrovatoException, IOException{
-       boolean isPresent = magazzino.stream().anyMatch(d->d.getId() == id);
-
-       if (!isPresent){
-           throw new ProdottoNonTrovatoException("Impossibile procedere: prodotto non trovato");
-       } else {
-           ProdottoElettronico tmp = filteredById(id);
-           MagazzinoReader.rimuoviProdottoMagazzino(tmp);
-           magazzino = MagazzinoReader.leggiMagazzinoDaFile();
-       }
+    public void removeProductFromMagazzino(int id){
+        ExceptionHandler.handlexception(()-> { boolean isPresent = magazzino.removeIf(d->d.getId() == id);
+            if (!isPresent){
+                throw new ProdottoNonTrovatoException("Impossibile procedere: prodotto non trovato");
+            } else {
+                ProdottoElettronico tmp = filteredById(id);
+                MagazzinoReader.rimuoviProdottoMagazzino(tmp);
+                magazzino = MagazzinoReader.leggiMagazzinoDaFile();
+            }
+            return null;
+        });
     }
 
-    public ProdottoElettronico filteredById( int id) throws ProdottoNonTrovatoException {
-         return magazzino.stream().filter(d-> d.getId() == id).findFirst().orElseThrow(()-> new ProdottoNonTrovatoException("Nessuna corrispondenza nel magazzino"));
+    public ProdottoElettronico filteredById( int id){
+        return ExceptionHandler.handlexception(()-> magazzino.stream()
+                .filter(d-> d.getId() == id)
+                .findFirst().orElseThrow(()-> new ProdottoNonTrovatoException("Nessuna corrispondenza nel magazzino")));
     }
 
     public Set<ProdottoElettronico> getMagazzino() {
@@ -95,7 +94,7 @@ public class Magazzino {
         this.magazzino = magazzino;
     }
 
-    public void decrementaQuantita(int id, int amount) throws ProdottoNonTrovatoException, IOException {
+    public void decrementaQuantita(int id, int amount){
         ProdottoElettronico prodotto = filteredById(id);
         int nuovaQuantita = prodotto.getQuantitaMagazzino() - amount;
         if (nuovaQuantita < 0) {
@@ -109,35 +108,17 @@ public class Magazzino {
         }
     }
 
-    public void incrementaQuantita(int id, int amount) throws ProdottoNonTrovatoException, IOException {
-
-        ProdottoElettronico prodotto = filteredById(id);
-        int nuovaQuantita = prodotto.getQuantitaMagazzino() + amount;
-        if (nuovaQuantita < 0) {
-            throw new IllegalArgumentException("Quantità non può essere negativa");
-        } else{
-            prodotto.setQuantitaMagazzino(nuovaQuantita);
-            MagazzinoReader.aggiornaMagazzino(magazzino);
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        if(magazzino == null | magazzino.isEmpty()){
-            return "Magazzino vuoto";
-        }
-        return "Magazzino{" +
-                "magazzino=" + magazzino +
-                '}';
-    }
-    @Override
-    public String toString() {
-        if(magazzino == null | magazzino.isEmpty()){
-            return "Magazzino vuoto";
-        }
-        return "Magazzino{" +
-                "magazzino=" + magazzino +
-                '}';
+    public void incrementaQuantita(int id, int amount){
+        ExceptionHandler.handlexception(()-> {
+            ProdottoElettronico prodotto = filteredById(id);
+            int nuovaQuantita = prodotto.getQuantitaMagazzino() + amount;
+            if (nuovaQuantita < 0) {
+                throw new IllegalArgumentException("Quantità non può essere negativa");
+            } else{
+                prodotto.setQuantitaMagazzino(nuovaQuantita);
+                MagazzinoReader.aggiornaMagazzino(magazzino);
+            }
+            return null;
+        });
     }
 }
