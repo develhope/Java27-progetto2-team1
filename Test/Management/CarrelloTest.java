e426a6ce48a77329de187bb9f5468b33e8997e91
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-09-09 16:56:54 +0200

    Aggiunto package per testing, generata classe per testing carrello con beforeEach e inizializzazione dei parametri

---
+++ Test/Management/CarrelloTest.java
@@ -0,0 +1,34 @@
+package Management;
+
+import Enums.TipoElettronico;
+import Products.Prodotto;
+import Products.ProdottoElettronicoUtente;
+import Users.Cliente;
+import Users.Utente;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarrelloTest {
+
+	private Carrello carrello;
+	ProdottoElettronicoUtente prd1;
+	ProdottoElettronicoUtente prd2;
+	Utente usr1 = new Cliente();
+	List<Utente> utenti;
+
+	@BeforeEach
+	void setUp() {
+		carrello = new Carrello();
+		prd1 = new ProdottoElettronicoUtente("Apple", "iPhone 14 pro", "Bello e potente", 1500.00, 0, TipoElettronico.SMARTPHONE, 6.3f);
+		prd2 = new ProdottoElettronicoUtente("Samsung", "pc portatile", "molto interessante", 870,1, TipoElettronico.NOTEBOOK, 15.0f);
+		usr1 = new Cliente("Piero", "Neri", 35, "piero@gmail.com", "piero13");
+		utenti = new ArrayList <>();
+		utenti.add(usr1);
+	}
+
+}
