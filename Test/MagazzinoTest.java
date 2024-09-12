51bd6e404cee9e070cc7e2d1a29200bab3d80700
Federica Parillo <federicaparillo.94@gmail.com>
2024-09-12 13:59:51 +0200

    Aggiunto message per ogni Test

--- Test/MagazzinoTest.java
+++ Test/MagazzinoTest.java
@@ -23,39 +23,39 @@ public class MagazzinoTest {
     @Test
     public void testAggiungiProdottoAlMagazzino() {
         magazzino.addProductToMagazzino(prodotto2);
-        assertEquals(2, magazzino.totaleProdotti());
+        assertEquals(2, magazzino.totaleProdotti(), "Il numero dei prodotti nel magazzino dovrebbe essere due");
     }
 
     @Test
     public void testRimuoviProdottoDaMagazzino() {
         magazzino.addProductToMagazzino(prodotto2);
         magazzino.removeProductFromMagazzino(1);
-        assertEquals(1, magazzino.totaleProdotti());
+        assertEquals(1, magazzino.totaleProdotti(), "Il numero dei prodotti nel magazzino dovrebbe essere uno");
     }
 
     @Test
     public void testFiltraPerTipo() {
         Set<ProdottoElettronico> filtred = magazzino.filtredBytype("Smartphone");
-        assertTrue(filtred.contains(prodotto1));
+        assertTrue(filtred.contains(prodotto1), "Il magazzino dovrebbe contenere il prodotto uno");
     }
 
     @Test
     public void testFiltraPerModello() {
         Set<ProdottoElettronico> filtrato = magazzino.filtredByModel("MacBook");
-        assertFalse(filtrato.contains(prodotto1));
+        assertFalse(filtrato.contains(prodotto1), "Il magazzino non dovrebbe contenere il prodotto uno");
     }
 
 
     @Test
     public void testMagazzinoNotNull(){
-       assertNotNull(magazzino.getMagazzino());
+       assertNotNull(magazzino.getMagazzino(), "Il magazzino non dovrebbe essere null");
     }
 
     @Test
     public void testCorrispondenzaContenuti(){
        magazzino.addProductToMagazzino(prodotto2);
        ProdottoElettronico [] expectedItems = {prodotto1, prodotto2};
-       assertArrayEquals(expectedItems, magazzino.getMagazzino().toArray());
+       assertArrayEquals(expectedItems, magazzino.getMagazzino().toArray(), "Il magazzino dovrebbe contenere il prodotto uno e il due");
     }
 
 }
