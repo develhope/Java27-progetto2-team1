f3b37667301878060cfd11167c8b2dd050b203d2
Federica Parillo <federicaparillo.94@gmail.com>
2024-09-13 11:09:15 +0200

    Aggiunta classe di Testing per Magazziniere e metodo setup per i test

---
+++ Test/MagazziniereTest.java
@@ -0,0 +1,29 @@
+import Management.Magazzino;
+import Products.ProdottoElettronico;
+import Users.Magazziniere;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class MagazziniereTest {
+    private ProdottoElettronico prodotto1;
+    private ProdottoElettronico prodotto2;
+    private Magazziniere magazziniere;
+
+
+
+    @BeforeEach
+    public void setup() {
+        magazziniere = new Magazziniere("Mario", "Rossi", 30, "mario.rossi@gmail.com", "1234");
+        magazziniere.getMagazzino().getMagazzino().clear();
+        prodotto1 = new ProdottoElettronico.ProdottoElettronicoBuilder("Samsung", "Galaxy", 500.00, 1, "Smartphone", 5.6f).setPrezzoVendita(900.00).build();
+        prodotto2 = new ProdottoElettronico.ProdottoElettronicoBuilder("Apple", "MacBook Air", 900.00, 2, "Notebook", 13f).setPrezzoVendita(1300.00).build();
+        magazziniere.addProductToMagazzino(prodotto1, 5);
+    }
+
+
+
+
+
+
+
+}
