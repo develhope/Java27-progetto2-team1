566fc9be81da236d7573335a5d27a6adbc976c3d
Federica Parillo <federicaparillo.94@gmail.com>
2024-09-13 12:49:48 +0200

    Aggiunta metodo svuota magazzino

--- src/Users/Magazziniere.java
+++ src/Users/Magazziniere.java
@@ -56,6 +56,10 @@ public class Magazziniere extends Utente {
     public Magazzino getMagazzino() {
         return magazzino;
     }
+
+    public void svuotaMagazzino(){
+        magazzino.svuotaMagazzino();
+    }
 }
 
 
